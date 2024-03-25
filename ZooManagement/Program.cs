9dee8a28eb5d250ec622d48eb0bd08720db15670
vutrn <meoiuwai@gmail.com>
2024-03-25 19:59:16 +0700

    ok

--- ZooManagement/Program.cs
+++ ZooManagement/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ZooManagement
+﻿
+namespace ZooManagement
 {
   internal class Program
   {
 	static void Main(string[] args)
 	{
-	  Console.WriteLine("Hello, World!");
+	  SqlConnection sqlConnection;
+	  string connectionString = @"Data Source=DESKTOP-KCB2BH6\MSSQLSERVER02;Initial Catalog=studentCRUD;Integrated Security=True";
+
+	  sqlConnection = new SqlConnection(connectionString);
+	  sqlConnection.Open();
+
+	  string delete = "delete record ";
+	  SqlCommand deletequery = new SqlCommand(delete, sqlConnection);
+
+	  deletequery.ExecuteNonQuery();
+
+
+
+
+	  sqlConnection.Close();
+
+
+
+
+
+	  Console.ReadKey();
 	}
   }
 }
